8beb7be3c7582c0de29ceaa80d8d0d1a3f428d51
3aeedHektor <3aeedhektor@gmail.com>
2024-02-03 03:23:43 +0330

    Getting data by user id (id=8)

--- app/src/main/java/com/example/jctest/api/ApiInterface.kt
+++ app/src/main/java/com/example/jctest/api/ApiInterface.kt
@@ -4,6 +4,7 @@ import com.example.jctest.models.User
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiInterface {
 
@@ -12,5 +13,8 @@ interface ApiInterface {
 
     @GET("posts")
     suspend fun getAllUsers(): Response<List<User>>
+
+    @GET("posts")
+    suspend fun getDataByUserId(@Query("userId") userId: Int): Response<List<User>>
 
 }
